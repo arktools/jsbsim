d9b143f218586bfcc9b425419588aad84534dba0
James Goppert <jgoppert@w7.hsl.dynalias.com>
2010-09-25 17:07:37 -0400

    Added aircraft option.

--- scicoslab/scripts/fgmrun.sh
+++ scicoslab/scripts/fgmrun.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #--native-ctrls=socket,out,119,,5501,udp \
 #--native-ctrls=socket,in,120,,5502,udp \
-if [[ $# != 2 ]]
+if [[ $# != 3 ]]
 then
-	echo usage: $0 ip-address num
+	echo usage: $0 ip-address num aircraft
 	exit
 fi
 ip=$1
 num=$2
+aircraft=$3
 port=$((5000+$num))
 callsign=f-$num
 echo callsign: $callsign
@@ -17,7 +18,7 @@ portFdm=5500+$num
 #--native-fdm=socket,in,120,,portFdm,udp \
 fgfs \
 --callsign=$callsign \
---aircraft=T38 \
+--aircraft=$aircraft \
 --geometry=400x300 \
 --multiplay=in,10,$ip,$port \
 --multiplay=out,10,mpserver05.flightgear.org,5000 \
