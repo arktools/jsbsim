4d180675ead23333b23717277e11f8914e3264a4
Lenna Peterson <lxpeters@purdue.edu>
2012-02-20 15:20:35 -0500

    Changed to use CMake's method (`exec` in bundle)

--- cmake/macFind.sh
+++ cmake/macFind.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # APPLE ONLY # 
 # This is intended to be CPACK_BUNDLE_STARTUP_COMMAND
-# (copied to $BUNDLE/Contents/Resources/MacOS/ and `exec`s the binary)
+# (copied to $BUNDLE/Contents/MacOS/ and `exec`s the binary)
 # `mdls $FILE` lists all metadata searchable by mdfind
-jsbsimTop=`mdfind "kMDItemKind == Application && kMDItemFSName == jsbsim.app" | head -1`
-echo $jsbsimTop
-f16xml=`mdfind -onlyin $jsbsimTop "kMDItemFSName == f16.xml"`
+
+# Finds app bundle
+#BUNDLE=`mdfind "kMDItemKind == Application && kMDItemFSName == jsbsim.app" | head -1`
+# CMake's method for finding this path (requires script to be run from $BUNDLE/Contents/MacOS/)
+BUNDLE=`echo "$0" | sed -e 's:/Contents/MacOS/.*::'`
+echo $BUNDLE
+f16xml=`mdfind -onlyin $BUNDLE "kMDItemFSName == f16.xml" | sed -e 's:share/jsbsim/aircraft/f16/f16.xml::'`
+# Outputs absolute path to location of bin/ share/ etc. 
 echo $f16xml
