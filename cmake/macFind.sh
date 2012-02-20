b5d6a085f3de406cab613e112aa23f972f4990b6
Lenna Peterson <lxpeters@purdue.edu>
2012-02-20 14:53:32 -0500

    Adding comments.

--- cmake/macFind.sh
+++ cmake/macFind.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# APPLE ONLY # 
+# This is intended to be CPACK_BUNDLE_STARTUP_COMMAND
+# (copied to $BUNDLE/Contents/Resources/MacOS/ and `exec`s the binary)
+# `mdls $FILE` lists all metadata searchable by mdfind
 jsbsimTop=`mdfind "kMDItemKind == Application && kMDItemFSName == jsbsim.app" | head -1`
 echo $jsbsimTop
 f16xml=`mdfind -onlyin $jsbsimTop "kMDItemFSName == f16.xml"`
