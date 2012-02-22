a8995d1be4f3741d0405cad22da6988884e73afe
Lenna Peterson <lxpeters@purdue.edu>
2012-02-21 22:23:30 -0500

    Binary script works on Darwin.

--- cmake/buildBinaries.py
+++ cmake/buildBinaries.py
@@ -8,25 +8,7 @@ import inspect # for finding current directory
 import subprocess # for calling processes
 
 # apparently __file__ doesn't work consistently on windows
-#print os.path.dirname(os.path.abspath(__file__))
 scriptDir = os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0])
-#subDir = os.path.join(scriptDir, "arkcmake")
-#if os.path.exists(subDir):
-    #sys.path.append(subDir)
-
-#try:
-    #from get_build_path import get_build_path
-#except ImportError:
-    #print "Could not find 'get_build_path.py'"
-    #print "This module is required."
-    #raise SystemExit
-
-#build_path = get_build_path()
-#if build_path:
-    #os.chdir(build_path)
-#else:
-    #print "The script was unable to find a build directory."
-    #raise SystemExit
 
 buildDir = os.path.normpath(os.path.join(scriptDir, "../build"))
 #print buildDir
@@ -46,24 +28,23 @@ def callCMake(*args):
     _cmakeCall = list(cmakeArgs)
     # Sets DWITH_GUI default to y
     if "-DWITH_GUI=n" not in args:
-        _cmakeCall.append("DWITH_GUI=y")
+        _cmakeCall.append("-DWITH_GUI=y")
     for a in args:
         _cmakeCall.append(a)
     _cmakeCall.insert(0, "cmake")
-    print _cmakeCall
-    #subprocess.call("rm -f CMakeCache.txt", shell=True)
-    #subprocess.call(_cmakeCall) 
-    #subprocess.call("make") 
+    #print _cmakeCall
+    subprocess.call("rm -f CMakeCache.txt", shell=True)
+    subprocess.call(_cmakeCall) 
+    subprocess.call(["make"]) 
 
 def callCPack(*args):
     _cpackCall = []
     for a in args:
         _cpackCall.append(a)
     _cpackCall.insert(0, "cpack")
-    print _cpackCall
-    #subprocess.call(_cpackCall)
+    #print _cpackCall
+    subprocess.call(_cpackCall)
  
-#print os.uname()[0]
 if sys.platform.startswith('darwin'):
     print "Building for Darwin"
     # WITH_OSXBUNDLE defaults to ON
